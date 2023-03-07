1c43f9ac82845108daee5207a840581e48a9bd84
freddymi1 <michelfreddy1992@gmail.com>
2023-03-07 16:18:29 +0300

    Fix error on deploy

--- src/pages/components/LoadingComponent/LoadingComponent.jsx
+++ src/pages/components/LoadingComponent/LoadingComponent.jsx
@@ -3,10 +3,10 @@ import ReactLoading from 'react-loading';
 export default function LoadingComponent() {
     const type = "spokes";
     const color = "hsl(var(--hue-color), 62%, 28%)";
-    
+
     return (
     <div className='__container'>
-        <ReactLoading type={type} color={color} height={'10%'} width={'10%'} />
+        <ReactLoading type={type} color={color} height={'5%'} width={'5%'} />
     </div>
   )
 }
