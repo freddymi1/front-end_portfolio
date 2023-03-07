4a1787912044df0685f29b314d7c05835973d9dd
freddymi1 <michelfreddy1992@gmail.com>
2023-03-07 16:50:03 +0300

    Fix scroll in swipper

--- src/pages/components/Portfolio/Portfolio.jsx
+++ src/pages/components/Portfolio/Portfolio.jsx
@@ -25,6 +25,7 @@ export default function Portfolio() {
             <Swiper 
                 keyboard={true}
                 grabCursor={true}
+                scrollbar={false}
                 spaceBetween= {48}
                 pagination={{ 
                     clickable: true ,
